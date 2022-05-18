58eb86f123606b8f110649cd6eeb1fae309b3c4f
louiswhite <louis2009.jlw@gmail.com>
2022-05-17 22:34:22 -0500

    commit file after listbox_task error

--- to_do_list.py
+++ to_do_list.py
@@ -20,18 +20,22 @@ def add_task():
         tkinter.messagebox.showwarning(title="Warning!", message="You must enter a task.")
 
 # Create GUI
-listbox_tasks = tkinter.Listbox(root, height=3, width=50)
-listbox_tasks.pack()
+frame_tasks = tkinter.Frame(root)
+frame_tasks.pack()
 
-scrollbar_tasks = tkinter.Scrollbar(root)
+scrollbar_tasks = tkinter.Scrollbar(frame_tasks)
 scrollbar_tasks.pack(side=tkinter.RIGHT, fill=tkinter.Y)
+
+listbox_tasks.config(yscrollcommand=scrollbar_tasks.set)
+scrollbar_tasks.config(command=listbox_tasks.yview)
+
+listbox_tasks = tkinter.Listbox(frame_tasks, height=3, width=50)
+listbox_tasks.pack(side=tkinter.LEFT)
 
 entry_task = tkinter.Entry(root, width=50)
 entry_task.pack()
 
 button_add_task = tkinter.Button(root, text="Add task", width=48, command=add_task)
 button_add_task.pack()
-
-
 
 root.mainloop()
